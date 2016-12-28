2097c47458df515c8e5a29beffa972b93f3076fe
Misha <askerka.m@gmail.com>
2016-12-28 16:20:26 +0300

    Testing the functional app on heroku

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ from bokeh.models import DatetimeTickFormatter
 from bokeh.layouts import row, widgetbox, column
 #from bokeh.models import ColumnDataSource,CustomJS
 #from bokeh.models.widgets import Slider, TextInput
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from datetime import datetime
+import colorsys
 
 app = Flask(__name__)
 app.vars={}
@@ -23,13 +26,14 @@ def main():
 def index():
     if request.method == 'GET':
         return render_template('my_index.html')
+        #return render_template('my_index_test.html')
     #else:
     if request.method == 'POST':
         #return render_template('my_index.html')
         #print('lalalal')
         app.vars['n_days'] = request.form['n_days']
-        app.vars['ticker'] = request.form['ticker']
-        print(app.vars['n_days'],app.vars['ticker'])
+        app.vars['ticker'] = request.form['ticker'].split(',')
+        #print(app.vars['n_days'],app.vars['ticker'])
         with open('{0}_{1}.txt'.format(app.vars['ticker'],app.vars['n_days']),'w') as f:
             f.write('Company Name: {0}\n'.format(app.vars['ticker']))
             f.write('Last N Days: {0}\n\n'.format(app.vars['n_days']))
@@ -41,14 +45,17 @@ def index():
 
 @app.route('/calculated_plot',methods=['GET','POST'])
 def calculated_plot():
-
-    a = pd.read_csv('https://www.quandl.com/api/v3/datasets/WIKI/{0}/data.csv'.format(app.vars['ticker']))
-    a['Date'] = pd.to_datetime(a['Date'])
-    mask = (a['Date'] > datetime.now().date() - timedelta(days=int(app.vars['n_days']))) & (a['Date'] <= datetime.now().date())
-    a_small = a.loc[mask]
-    print(a_small)
-    p = figure(title="Share prices for the last {0} days".format(app.vars['n_days']), x_axis_label='Date', y_axis_label='Price / USD')
-    p.line(a_small['Date'], a_small['Close'], legend= '{0}'.format(app.vars['ticker']),color='purple',line_width=2)
+    #HSV_tuples = [(x*1.0/N, 0.5, 0.5) for x in range(len(app.vars['ticker']))]
+    #RGB_tuples = list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples))
+    colors = ["#%02x%02x%02x" % (int(r), int(g), int(b)) for r, g, b, _ in 255*plt.cm.rainbow(mpl.colors.Normalize()(range(len(app.vars['ticker']))))]
+    p = figure(title="Stock share prices for the last {0} days".format(app.vars['n_days']), x_axis_label='Date', y_axis_label='Price / USD')
+    for i, ii in enumerate(app.vars['ticker']):
+        print(ii)
+        a = pd.read_csv('https://www.quandl.com/api/v3/datasets/WIKI/{0}/data.csv'.format(ii))
+        a['Date'] = pd.to_datetime(a['Date'])
+        mask = (a['Date'] > datetime.now().date() - timedelta(days=int(app.vars['n_days']))) & (a['Date'] <= datetime.now().date())
+        a_small = a.loc[mask]
+        p.line(a_small['Date'], a_small['Close'],color=colors[i], legend= '{0}'.format(ii),line_width=2)
     #p.line(x, y, legend= 'FB',color='purple',line_width=2)
     p.xaxis.formatter=DatetimeTickFormatter(formats=dict(
             hours=["%d %B %Y"],
